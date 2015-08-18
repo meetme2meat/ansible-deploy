50a4c7de1ec85e65b6f2cea6c0226b180490787d
Vignesh Selvaraj <s.vignesh07@gmail.com>
2015-08-18 11:00:41 +0530

    Ajax for signin and sign up forms

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -18,8 +18,7 @@ var Michef = (function (Michef) {
   		$("#user_login_button").on('click', function() {
   		  $('#main_modal').modal('show');
   		  $('.login_modal').show();
-  		  $('.forgot_password_modal').hide();
-  		  $('.signup_modal').hide();
+  		  $('.forgot_password_modal, .signup_modal').hide();
   		});
   		/*click on forgot password link hide login and signup modal*/
   		$("#forgot_password").on('click',function(){
@@ -30,18 +29,54 @@ var Michef = (function (Michef) {
   		/*click on sign up link hide login and forgot modal*/
   		$('.sign_up').on('click',function(){
   		  event.preventDefault();
-  		  $('.forgot_password_modal').hide();
-  		  $('.login_modal').hide();
+        $('error-message').hide();
+  		  $('.forgot_password_modal, .login_modal').hide();
   		  $('.signup_modal').show();
   		});
   		/*click on log in link hide sign up and forgot modal*/
   		$('#log_in').on('click',function(){
   		  event.preventDefault();
-  		  $('.forgot_password_modal').hide();
+  		  $('.forgot_password_modal, .signup_modal').hide();
   		  $('.login_modal').show();
-  		  $('.signup_modal').hide();
   		});
-  });
 
+      $('.email-signup').submit(function(e) {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+
+        $.ajax({
+            url: $(this).attr('action'),
+            method: $(this).attr('method'),
+            data: $(this).serialize(),
+            success: function (response) {
+              $('.email-signup').html($(response).find('.email-signup').html());
+            },
+            error: function () {
+              $('.registration_form_container').html("Error");
+            }
+         });
+        return false;
+      });
+
+      $('.email-signin').submit(function(e) {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+
+        $.ajax({
+            url: $(this).attr('action'),
+            type: $(this).attr('method'),
+            data: $(this).serialize(),
+            success:function(){
+                location.reload();
+            },
+            error: function() {
+              $('.error_message_login').html('Invalid Username or Password').show();
+              setTimeout(function() {
+                $('.error_message_login').hide();
+              }, 3000);
+            }
+        });
+    });
+  });
   return Michef;
 })(Michef);
